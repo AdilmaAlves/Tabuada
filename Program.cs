9c645023d8732341b58c7c6ca3a2e9c1d971565e
Adilma Alves <125414087+AdilmaAlves@users.noreply.github.com>
2023-02-14 22:07:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int numero,resultado;
+Console.WriteLine("Digite um número para ver a Tabuada dele:");
+numero = int.Parse(Console.ReadLine());
+
+for(int i = 0; i <11; i++){
+    resultado = numero*i;
+    Console.WriteLine($"{numero} X {i} = {resultado}");
+}
